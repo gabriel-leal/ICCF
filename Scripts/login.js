ceaf20f32250139cf321dde7f08c70aa60f97a39
gabriel-leal <gabrielealmenezes@gmail.com>
2024-06-01 14:54:26 -0300

    atualização

--- Scripts/login.js
+++ Scripts/login.js
@@ -2,14 +2,3 @@ login = document.getElementById('ilogin');
 senha = document.getElementById('isenha');
 vali = document.getElementById('ivali');
 esqueci = document.getElementById('esquecisenha');
-
-
-
-
-
-
-
-
-function esquecisenha() {
-
-}
