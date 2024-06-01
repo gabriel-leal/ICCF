c98f516492bad472a23bf47a776c5e44b9277728
gabriel-leal <gabrielealmenezes@gmail.com>
2024-06-01 09:50:01 -0300

    login

---
+++ Scripts/login.js
@@ -0,0 +1,15 @@
+login = document.getElementById('ilogin');
+senha = document.getElementById('isenha');
+vali = document.getElementById('ivali');
+esqueci = document.getElementById('esquecisenha');
+
+
+
+
+
+
+
+
+function esquecisenha() {
+
+}
