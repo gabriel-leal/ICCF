9693d8c99b3d765914fe7a5cc5618ddcc2edd7eb
gabriel-leal <gabrielealmenezes@gmail.com>
2024-06-07 14:50:17 -0300

    recuperação de senha (beta)

--- Scripts/esquecisenha.js
+++ Scripts/esquecisenha.js
@@ -1,16 +1,5 @@
 document.getElementById('formulario').addEventListener('submit', function(event) {
     event.preventDefault();
-    const password = document.getElementById('insenha').value;
-    const confirmPassword = document.getElementById('icnsenha').value;
-    const message = document.getElementById('msg');
-
-    if (password !== confirmPassword) {
-        message.textContent = 'As senhas não coincidem.';
-        message.classList.remove('success');
-        message.classList.add('error');
-    } else {
-        message.textContent = 'Cadastrado com Sucesso!';
-        message.classList.remove('error');
-        message.classList.add('succes');
-    }
+    email = document.getElementById('imail')
+    
 })
